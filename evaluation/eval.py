58e429247b290b60ba214c647974e46eea5b8a8d
Maren <al236@stud.uni-heidelberg.de>
2025-02-12 19:32:35 +0100

    update from train12.pt to train14.pt

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO("model/train12.pt")
+model = YOLO("model/train14.pt")
 
 # Start evaluation on test dataset
 model.val(data="eval.yaml", imgsz=640, device="cpu", split="test")
--- training/train.py
+++ training/train.py
@@ -1,8 +1,6 @@
 from ultralytics import YOLO
 
-# Load model
 model = YOLO("yolo11n.pt")
 
-
-# Train model
-model.train(data="train.yaml", imgsz=640, batch=8, epochs=250, workers=1, device="cpu")
+#model.train(data="dataset_custom.yaml", imgsz=640, batch=8, epochs=50, workers=1, device="cpu")
+model.train(data="dataset_custom.yaml", imgsz=640, batch=8, epochs=100, workers=1, device="cpu")
